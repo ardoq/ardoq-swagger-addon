ff52f1f2cc0e83a08ad6ae2beee0412af7021753
Sigurhjörtur Snorrason <sigurhjortur@ardoq.com>
2015-06-30 16:17:36 +0200

    Added the common file

---
+++ src/ardoq/swagger/common.clj
@@ -0,0 +1,51 @@
+(ns ardoq.swagger.common
+  (:require [ardoq.swagger.client :as api]
+            [cheshire.core :refer [generate-string parse-string]]
+            [clojure.java.io :as io]
+            [clojure.string :as s]
+            [clojurewerkz.urly.core :as urly]
+            [clostache.parser :as tpl]
+            [medley.core :refer [map-vals]]
+            [clj-http.client :as http]))
+
+(defn find-or-create-model [client type]
+  (if-let [model (first (filter #(= type (:name %)) (api/find-all (api/map->Model {}) client)))]
+    model
+    (-> (api/map->Model (parse-string (slurp (io/resource (if (= type = "Swagger") "modelv1.json" "modelv2.json"))) true))
+        (api/create client))))
+
+(defn- field-exists? [client field-name {:keys [_id] :as model}]
+  (not (empty? (filter
+                (fn [{:keys [name model]}]
+                  (and (= name field-name)
+                       (= model (str _id))))
+                (api/find-all (api/map->Field {}) client)))))
+
+(defn find-or-create-fields [client {model-id :_id :as model}]
+  (when-not (field-exists? client "method" model)
+    (-> (api/->Field "method" "method" "Text" (str model-id) [(api/type-id-by-name model "Operation")])
+        (api/create client)))
+  (when-not (field-exists? client "produces" model)
+    (-> (api/->Field "produces" "produces" "List" (str model-id) [(api/type-id-by-name model "Operation") (api/type-id-by-name model "Resource")])
+        (api/create client)))
+  (when-not (field-exists? client "consumes" model)
+    (-> (api/->Field "consumes" "consumes" "List" (str model-id) [(api/type-id-by-name model "Operation") (api/type-id-by-name model "Resource")])
+        (api/create client))))
+
+(defn generate-operation-description [data models]
+  (reduce
+   (fn [description [model-id {:keys [_id] :as model}]]
+     (s/replace description (re-pattern (str "\\|" (name model-id) "\\|")) (str "|[" (name model-id) "](comp://" _id ")|")))
+   (tpl/render-resource "operationTemplate.tpl" data)
+   models))
+
+(defn update-comp [client component {:keys [produces consumes]}]
+  ;; Updates a component based on previous modelling. Uses the swagger file to detect what it needs. 
+  (api/update 
+   (cond-> (api/map->Component component)
+     produces (assoc :produces produces)
+     consumes (assoc :consumes consumes)) client))
+
+(defn save-models [models client]
+  (map-vals #(let [schema (:schema %)]
+               (assoc (api/create (dissoc % :schema) client) :schema schema)) models))
