d05505b6ea74b004d8e9647cc31965940212b742
Sigurhjörtur Snorrason <sigurhjortur@ardoq.com>
2015-07-03 10:45:52 +0200

    Made a manual scraper through the key companies

--- src/ardoq/swagger/scrape.clj
+++ src/ardoq/swagger/scrape.clj
@@ -15,22 +15,28 @@
                        :token "2330f05eac3846f78a13b01930099b97"
                        :org "ardoq"}))
 
-(defn google-urls []
-  (let [{:keys [apis]} (api/get-resource-listing "http://theapistack.com/data/google/apis.json" nil)]
-    (doseq [k apis]
-      (doseq [prop (get-in k [:properties])]
-        (if (= (get-in prop [:type]) "Swagger")
-          (api/get-spec client (get-in prop [:url]) nil nil nil))))))
+(defn get-company [{:keys [apis]}]
+  (println apis)
+  (doseq [k apis]
+    (doseq [prop (get-in k [:properties])]
+      (if (= (get-in prop [:type]) "Swagger")
+        (try
+          (api/get-spec client (get-in prop [:url]) nil nil nil)
+          (catch Exception e (println (.getMessage e))))))))
+
+(def url-list ["3scale" "alchemyapi" "altos-data-geeks" "angellist" "bistri" "crowdin" "digitalgov-search" "dossia-" "dwolla" "easycron" "eventbrite" "expedia" "fueleconomygov" "gis-cloud" "google" "help-scout" "information-sharing-environment" "linkedin" "meetup-api" "nounproject-dev-team" "opencorporates" "paypal" "pinboard" "reddit" "runscope" "slack-api" "soundcloud-api" "stack-exchange" "twitter"])
 
 (defn scrape-swaggers []
-  (let [listing (into [] (api/get-resource-listing "http://stack.apievangelist.com/data/companies.json" nil))
-        urls (find-keys listing (keyword "swagger-url"))]
-    (doseq [url urls]
-      (if (not (clojure.string/blank? url))
-        (try
-          (api/get-spec client url nil nil nil)
-          (catch Exception e (println (str "Failed retrieving " url "Exception: " (.getMessage e))))))))
-  (google-urls))
+  (doseq [url url-list]
+    (let [full-url (str "http://theapistack.com/data/" url "/apis.json")
+          listing (into [] (api/get-resource-listing full-url nil))
+          urls (find-keys listing (keyword "swagger-url"))]
+      (doseq [url urls]
+        (if (not (clojure.string/blank? url))
+          (try
+            (api/get-spec client url nil nil nil)
+            (catch Exception e (println (str "Failed retrieving " url "Exception: " (.getMessage e)))))))
+      (get-company (into {} listing)))))
 
 (defn get-selected [selected]
   (api/get-spec client selected nil nil nil))
