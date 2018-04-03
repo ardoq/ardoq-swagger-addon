98653257db336dd1c7160420cb2d67aaa5fd29ea
Kristian Helgesen <kristian@ardoq.com>
2018-04-03 22:11:19 +0200

    Release 1.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ardoq-swagger-addon "1.0.10"
+(defproject ardoq-swagger-addon "1.0.11"
   :description "Swagger add-on for Ardoq"
   :url "http://ardoq.com"
   :license {:name "Eclipse Public License"
