30fa05f3fcaca1dab41573c3fd22921eb20f7007
David Russell <david.russell@ardoq.com>
2021-05-18 14:06:20 -0400

    Conditionally push latest tag

--- .circleci/deploy-docker.sh
+++ .circleci/deploy-docker.sh
@@ -15,11 +15,24 @@ RESPONSE=$(curl --write-out %{http_code} --silent --output /dev/null -H "Authori
 if [ $RESPONSE -eq 404 ]; then
     echo "Building docker container..."
     docker build -t $REPOSITORY:$DOCKER_TAG .
-    docker tag $REPOSITORY:$DOCKER_TAG $REPOSITORY:latest
+    if [ "${CIRCLE_BRANCH}" == "master" ]; then
+      docker tag $REPOSITORY:$DOCKER_TAG $REPOSITORY:latest
+    elif [ "${CIRCLE_BRANCH}" == "test" ]; then
+      docker tag $REPOSITORY:$DOCKER_TAG $REPOSITORY:test
+    elif [ "${CIRCLE_BRANCH:0:12}" = "internal-env" ]; then
+      docker tag $REPOSITORY:$DOCKER_TAG
+    fi
+
     echo "Pushing $REPOSITORY:$DOCKER_TAG to private registry..."
     echo "{\"https://index.docker.io/v1/\":{\"auth\":\"$DOCKER_AUTH\",\"email\":\"$DOCKER_EMAIL\"}}" >> ~/.dockercfg
     docker push $REPOSITORY:$DOCKER_TAG
-    docker push $REPOSITORY:latest
+
+    if [ "${CIRCLE_BRANCH}" == "master" ]; then
+      docker push $REPOSITORY:latest
+    elif [ "${CIRCLE_BRANCH}" == "test" ]; then
+      docker push $REPOSITORY:test
+    fi
+
     exit 0
 elif [ $RESPONSE -eq 200 ]; then
     echo "Can't deploy version $REPOSITORY:$DOCKER_TAG, version already exists!"
