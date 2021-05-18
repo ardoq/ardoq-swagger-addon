dfb26808efef587f9de035d4f5fe4ad59979b684
David Russell <david.russell@ardoq.com>
2021-05-18 12:35:55 -0400

    Release 1.0.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ardoq-swagger-addon "1.0.12"
+(defproject ardoq-swagger-addon "1.0.13"
   :description "Swagger add-on for Ardoq"
   :url "http://ardoq.com"
   :license {:name "Eclipse Public License"
